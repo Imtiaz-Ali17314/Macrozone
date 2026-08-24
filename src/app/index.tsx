48744c0242a2341571d93ebf9c02c0630a8fba09
Imtiaz Ali <imtiazali17314@gmail.com>
2026-08-24 15:55:54 +0500

    feat: enhance navigation and layout by adding Meals and Add Meal screens, and updating HomeScreen layout

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,20 +1,16 @@
-import { StyleSheet, Text, View } from "react-native";
-import { globalStyles } from "../styles/global";
+import { globalStyles } from "@/styles/global";
+import { Link } from "expo-router";
+import { ScrollView, Text } from "react-native";
+import HomeHeader from "../components/HomeHeader";
 
 export default function HomeScreen() {
   return (
-    <View style={globalStyles.container}>
+    <ScrollView style={globalStyles.container}>
       <Text style={globalStyles.title}>MacroZone</Text>
-      <Text style={styles.date}>Monday, March 16</Text>
-    </View>
+      <HomeHeader />
+      <Link href="/add-meal" style={{ fontSize: 18, color: "#007bff" }}>
+        Go to Add Meal
+      </Link>
+    </ScrollView>
   );
 }
-
-const styles = StyleSheet.create({
-  date: {
-    fontSize: 14,
-    color: "#a0a0b0",
-    marginTop: 4,
-    marginBottom: 30,
-  },
-});
